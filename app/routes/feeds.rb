ea9b769cc14733c6a7423a03b385fd71189d72a8
Timothy Hahn <timyhahn@gmail.com>
2014-07-26 00:00:17 -0400

    Added Feed routes

--- app/routes/feeds.rb
+++ app/routes/feeds.rb
@@ -1,6 +1,7 @@
 module Wellfed
   module Routes
     class Feeds < Sinatra::Application
+      # All Feeds
       get '/feeds/?' do
         Feed.all().to_json()
       end
@@ -8,13 +9,30 @@ module Wellfed
       post '/feeds/?' do
         title = params[:title]
         url = params[:url]
-        feed = Feed.create(:title => title, :url => url, :updated_at => Time.now, :latest_title => "!UNMINED!")
+        Feed.create(:title => title, :url => url, :updated_at => Time.now, :latest_title => "!UNMINED!").to_json()
       end
 
       delete '/feeds/?' do
         Feed.delete_all().to_json()
       end
 
+      # Specific Feed
+      get '/feeds/:feed_id' do
+        feed_id = params[:feed_id]
+        Feed.all(:id => feed_id).to_json()
+      end
+
+      put '/feeds/:feed_id' do
+        feed_id = params[:feed_id]
+        title = params[:title]
+        url = params[:url]
+        Feed.update(feed_id, :title => title, :url => url, :updated_at => Time.now)
+      end
+
+      delete '/feeds/:feed_id' do
+        feed_id = params[:feed_id]
+        Feed.destroy(feed_id)
+      end
     end
   end
 end
